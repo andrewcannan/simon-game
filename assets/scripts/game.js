47717c7ab77eba01e80c51dc0b8d380b74566a3e
andrewcannan <andrewmcannan@gmail.com>
2023-04-03 21:55:54 +0000

    added tests and code for game object, new game function, and some gameplay

---
+++ assets/scripts/game.js
@@ -0,0 +1,32 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"]
+}
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function lightsOn(circle) {
+    document.getElementById(circle).classList.add('light');
+    setTimeout(() => {
+        document.getElementById(circle).classList.remove('light')
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById('score').innerText = game.score;
+}
+module.exports = {game, newGame, showScore, addTurn, lightsOn}
